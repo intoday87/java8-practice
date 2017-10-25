ca5abae244ed06448f227916bac786cc1e0cdfcb
이동호(Nick) <intoday87@coupang.com>
2017-10-25 22:19:52 +0900

    java.time.* 에 관한 테스트 케이스 추가

--- src/test/java/DateTest.java
+++ src/test/java/DateTest.java
@@ -2,14 +2,15 @@ import org.junit.Test;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoField;
-import java.time.temporal.TemporalAdjusters;
 import java.time.temporal.UnsupportedTemporalTypeException;
+import java.util.Locale;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 public class DateTest {
     @Test
@@ -101,5 +102,76 @@ public class DateTest {
 
         assertThat(adjusted.getDayOfWeek(), equalTo(DayOfWeek.TUESDAY));
         assertThat(adjusted.getDayOfMonth(), equalTo(17));
+    }
+
+    @Test
+    public void dateTimeFormatter() throws Exception {
+        LocalDate date = LocalDate.of(2017, 1,18); // 월의 앞자리를 0으로 채우는지 확인하기 위해
+        assertThat(date.format(DateTimeFormatter.BASIC_ISO_DATE), equalTo("20170118"));
+        assertThat(date.format(DateTimeFormatter.ISO_LOCAL_DATE), equalTo("2017-01-18"));
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate date1 = LocalDate.of(2017, 10, 18);
+        String formattedDate = date1.format(formatter);
+        LocalDate date2 = LocalDate.parse(formattedDate, formatter);
+
+        assertThat(formattedDate, equalTo("18/10/2017"));
+        assertTrue(date1.equals(date2));
+        assertFalse(date1.equals(LocalDate.of(2017, 10, 19)));
+    }
+
+    @Test
+    public void dateTimeFormatterOfPattern() throws Exception {
+       DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("d. MMMM yyyy", Locale.KOREA);
+       LocalDate date = LocalDate.of(2017, 10, 18);
+       String formattedDate = date.format(dateTimeFormatter);
+       LocalDate date1 = LocalDate.parse(formattedDate, dateTimeFormatter);
+
+       assertThat(formattedDate, equalTo("18. 10월 2017"));
+       assertTrue(date.equals(date1));
+    }
+
+    @Test
+    public void dateTimeFormatterBuilder() throws Exception {
+        DateTimeFormatter dateTimeFormatter = new DateTimeFormatterBuilder()
+                .appendText(ChronoField.DAY_OF_MONTH)
+                .appendLiteral(". ")
+                .appendText(ChronoField.MONTH_OF_YEAR)
+                .appendLiteral(" ")
+                .appendText(ChronoField.YEAR)
+                .parseCaseSensitive()
+                .toFormatter(Locale.KOREA);
+
+        LocalDate date = LocalDate.of(2017, 10, 18);
+
+        assertThat(date.format(dateTimeFormatter), equalTo("18. 10월 2017"));
+    }
+
+    @Test
+    public void zoneId() throws Exception {
+        ZoneId seoulZoneId = ZoneId.of("Asia/Seoul");
+        assertNotNull(seoulZoneId);
+
+        LocalDateTime dateTime = LocalDateTime.of(2017, Month.APRIL, 10, 0, 0);
+        dateTime.atZone(seoulZoneId).toInstant();
+
+        Instant instant = Instant.now();
+        LocalDateTime ofInstant = LocalDateTime.ofInstant(instant, seoulZoneId);
+    }
+
+    @Test
+    public void zoneOffset() throws Exception {
+        ZoneOffset of = ZoneOffset.of("-09:00");
+        LocalDateTime now = LocalDateTime.now(of);
+
+        System.out.println(now);
+    }
+
+    @Test
+    public void offsetDateTime() throws Exception {
+        LocalDateTime dateTime = LocalDateTime.of(2017, Month.OCTOBER, 23, 22, 41);
+        OffsetDateTime.of(dateTime, ZoneOffset.of("+09:00"));
+
+        assertThat(dateTime.getHour(), equalTo(22));
     }
 }
